2b7e3e983a125dc1ca0afe376c914a9e83c96997
Nicole Souvenir <github@nicolesouvenir.com>
2017-11-19 02:29:17 -0500

    Completed storyboard and UI elements, completed stocks portion with autoLayout

---
+++ AC-iOS-U3W1HW-Parsing/Supporting Files/Models/StockInfo.swift
@@ -0,0 +1,62 @@
+//
+//  StockInfo.swift
+//  AC-iOS-U3W1HW-Parsing
+//
+//  Created by C4Q on 11/16/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import Foundation
+///Downcasting
+
+class StockInfo {
+    //what properties do we want to include
+    let date: String?
+    let openingAmount: Double
+    let closingAmount: Double
+    
+    init(date: String?, openingAmount: Double, closingAmount: Double) {
+        self.date = date
+        self.openingAmount = openingAmount
+        self.closingAmount = closingAmount
+    }
+    
+    //failable convenience initialzer
+    //how to make a stock profile from a JSON Dictionary mapping [String: Any]
+    convenience init?(from jsonDict: [String:Any]){
+        //my decision: if we don't have an openingAmount or closingAmount then do not make the stock profile at all
+        //name in jsonDict must match the name of the key in the json file
+        guard
+            let openingAmount = jsonDict["open"] as? Double,
+            let closingAmount = jsonDict["close"] as? Double
+            else{
+                return nil
+        }
+        let date = jsonDict["date"] as? String
+        
+        //go deeper and get any other information if needed
+        
+        self.init(date: date, openingAmount: openingAmount, closingAmount: closingAmount)
+    }
+    
+    //Parsing how to turn data into an array of stock profiles
+    static func createArrayOfStock(from data: Data) -> [StockInfo]? { //possibility of not finding an [StockInfo]
+        var stockProfile: [StockInfo] = []
+        //serialize buy converting json into foundation
+        //put in a do catch
+        ///what is this saying!!!!?!?!?!?!
+        do {
+            // do make sure that stockJSONArray tries to serailize the json with data as an array of dictionaries mapping String: Any. If not, return nil
+            guard let stockJSONArray = try JSONSerialization.jsonObject(with: data, options: []) as? [[String:Any]] else {return nil}
+            //turning into object: iterate through array and append stock info into stock profile
+            for stockDict in stockJSONArray{
+                if let thisStock = StockInfo(from: stockDict){
+                    stockProfile.append(thisStock)
+                }
+            }
+        } catch {
+            print("Error converting data to JSON")
+        }
+        return stockProfile
+    }
+}
---
+++ AC-iOS-U3W1HW-Parsing/Supporting Files/View Controllers/StocksTableViewController.swift
@@ -0,0 +1,68 @@
+//
+//  StocksTableViewController.swift
+//  AC-iOS-U3W1HW-Parsing
+//
+//  Created by C4Q on 11/16/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class StocksTableViewController: UIViewController {
+    var stockInfo = [StockInfo]()
+    
+    @IBOutlet weak var stocksTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stocksTableView.dataSource = self
+        getStockData()
+    }
+    
+    func getStockData(){
+        if let path = Bundle.main.path(forResource: "applstockinfo", ofType: "json"){
+            let myUrl = URL(fileURLWithPath: path)
+            if let data = try? Data(contentsOf: myUrl){
+                let stocks = StockInfo.createArrayOfStock(from: data)
+                self.stockInfo = stocks!
+            }
+        }
+        for stock in stockInfo{
+            print(stock.date ?? "no date available", stock.openingAmount, stock.closingAmount)
+        }
+    }
+}// end of class
+
+
+extension StocksTableViewController: UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stockInfo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Stock Cell", for: indexPath)
+        
+        let stockProfile = stockInfo[indexPath.row]
+        cell.textLabel?.text = stockProfile.date
+        cell.detailTextLabel?.text = String("Open: \(stockProfile.openingAmount)")
+        
+        return cell
+    }
+    
+    //MARK: - Segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? DetailStocksViewController {
+            //set up properties to send over
+            destination.stocks = stockInfo[stocksTableView.indexPathForSelectedRow!.row]
+        }
+    }
+}
+
+
+
