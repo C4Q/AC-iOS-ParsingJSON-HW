b96523c007f9b3b3a8b5bd0b407028042f5b35a4
Melissa He <melissahe@ac.c4q.nyc>
2017-11-20 03:37:57 -0500

    Add contacts tableview with working search bar

---
+++ AC-iOS-U3W1HW-Parsing/Contacts/View Controllers/ContactsDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ContactsDetailViewController.swift
+//  AC-iOS-U3W1HW-Parsing
+//
+//  Created by C4Q on 11/19/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class ContactsDetailViewController: UIViewController {
+
+    var person: Person!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI() {
+        
+        setupImages()
+    }
+    
+    func setupImages() {
+        
+    }
+
+}
---
+++ AC-iOS-U3W1HW-Parsing/Contacts/Views/ContactsTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsTableViewCell.swift
+//  AC-iOS-U3W1HW-Parsing
+//
+//  Created by C4Q on 11/19/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class ContactsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var contactImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+}
